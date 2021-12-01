cd76425748ea0f065be6738aa09c22cbf9900e26
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-01 10:53:42 +0900

    操作指示（仮）のDBへの保存が完成

--- applianceControl.py
+++ applianceControl.py
@@ -4,25 +4,34 @@ from flaskr.databases.collection_models.queueOperation import queueOperation
 #操作キューデータベースに上げと下げ操作を格納する
 #設定温度は{目標温度 : tTarget}で操作
 
-#指示をdbに保存
-#後でちゃんとdbの構造を見ながら書く
 def order_save(order):
-    queueOperation("ここに入れる値をいろいろ書く").save()
+    queueOperation(
+        operation=order
+        ).save()
+
+    return order
 
 
 #温度が閾値より高くなった時の指示
 def tempHigh(tTarget):
-    return "下げる"
+    order = "下げる",tTarget
+
+    res = order_save(str(order))
+    return res
+    #return order
 
 #温度が閾値より低くなった時の指示
 def tempLow(tTarget):
-    return "上げる"
+    order = "上げる",tTarget
+
+    res = order_save(str(order))
+    return res
+    #return order
 
 ##################################
 
 
 #######################温度の差を確認
-#sNumberいる？
 def control(tActual, tTarget):
     try: 
 
@@ -40,10 +49,12 @@ def control(tActual, tTarget):
         
         else: #範囲内
             res = "tolerance"
+        
+    
 
     except Exception as e:
         print(e)#エラー
+        res = "エラー"
 
     return res
 
-########################################
