b4c7900cddc840a685787f3faa91150f2188ba04
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-02 14:07:57 +0900

    操作キューデータベースへの保存完成

--- applianceControl.py
+++ applianceControl.py
@@ -1,59 +1,133 @@
 from flaskr.databases.collection_models.queueOperation import queueOperation
+import traceback
+import encodings
+import binascii
 
 ##################################
-#操作キューデータベースに上げと下げ操作を格納する
-#設定温度は{目標温度 : tTarget}で操作
 
-def order_save(order):
-    queueOperation(
-        operation=order
+
+def order_save(appliance, protocol, data, size, frequency = None):
+
+    #dataはintで渡されている
+    #文字列にして、bytesにしてからDBへ
+    data_str = str(data)
+    data = binascii.hexlify(data_str.encode('utf-8'))
+
+    if frequency == None:#NEC、家製協
+        queueOperation(
+        appliance = appliance,
+        protocol = protocol,
+        data = data,
+        size = size
         ).save()
 
-    return order
+    else:#その他
+        queueOperation(
+        appliance = appliance,
+        protocol = protocol,
+        data = data,
+        size = size,
+        frequency = frequency
+        ).save()
+    
 
+    print(appliance, protocol, data, size, frequency)
+
+    return "/save"
+
+
+#家電ごとに変わる(appliance, protocol),
+#操作指示ごとに変わる(size, frequency)
+
+
+class circulator:
+    code = {
+        "学校のサーキュレータ":{
+            "ON" : 0x17B00FF,
+            "OFF" : 0x17B00FF,
+            "Low" : 0x17B08F7,
+            "Mid" : 0x17B30CF,
+            "High" : 0x17B10EF,
+            "Shake" : 0x17B906F
+        }
+    }
+    def __init__(self, name, prot, size):
+        self.name = name
+        self.prot = prot
+        self.size = size
+    
+    def getInfo(self, ope):
+        return circulator.code[self.name][ope]
+    
+cir = circulator("学校のサーキュレータ", 3, 64)
 
 #温度が閾値より高くなった時の指示
-def tempHigh(tTarget):
-    order = "下げる",tTarget
+def tempHigh(tActual,tTargetHigh):
+    #電源オフ##########################
+    ope = "OFF"
 
-    res = order_save(str(order))
-    return res
-    #return order
+    ##サーキュレータ##
+    data = cir.getInfo(ope)
+    res = order_save("学校のサーキュレータ", 3, data, 64)
+    return res + " <- 電源オフ"
 
 #温度が閾値より低くなった時の指示
-def tempLow(tTarget):
-    order = "上げる",tTarget
+def tempLow(tActual,tTargetLow):
+    #電源オン##############################
+    ope = "ON"
+    
+    ##サーキュレータ##
+    data = cir.getInfo(ope)
+    res = order_save("学校のサーキュレータ", 3, data, 64)
 
-    res = order_save(str(order))
-    return res
-    #return order
+    res = res + " <- 電源オン"
 
-##################################
+    #強中弱#################################
+    #指定の温度は適当
+    difference = tTargetLow - tActual
+    if difference > 3:
+        ope = "Low"
+    elif difference > 5:
+        ope = "Mid"
+    else:
+        ope = "High"
+    
+    
+    ##サーキュレータ##
+    data = cir.getInfo(ope)
+    res = res + order_save("学校のサーキュレータ", 3, data, 64)
+    
+    return res + " <- 強さ設定"
 
 
-#######################温度の差を確認
+#####################温度の差を確認##########################
 def control(tActual, tTarget):
     try: 
+        res = None
 
-        threshold = 3   #目標値から閾値までの絶対値
+        threshold = 2   #目標値から閾値までの絶対値
 
-        tTagetHigh = tTarget + threshold
-        tTagetLow = tTarget - threshold
+        tTargetHigh = tTarget + threshold
+        tTargetLow = tTarget - threshold
 
         #######温度比較
-        if tActual > tTagetHigh:
-            res = tempHigh(tTarget)
+        if tActual > tTargetHigh:
+            res = tempHigh(tActual,tTargetHigh)
+
+            res = res + " <- 温度が高い"
         
-        elif tActual < tTagetLow:
-            res = tempLow(tTarget)
+        elif tActual < tTargetLow:
+            res = tempLow(tActual,tTargetLow)
+
+            res = res + " <- 温度が低い"
         
         else: #範囲内
-            res = "tolerance"
+            res = " <- ちょうどいい"
         
     
 
     except Exception as e:
-        print(e)#エラー
+        traceback.print_exc()#エラー
         res = "エラー"
 
     return res
