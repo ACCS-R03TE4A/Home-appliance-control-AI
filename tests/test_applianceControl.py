36ecff0d8a83ad6602ab82da7dd618341b877169
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-08 15:38:04 +0900

    テスト修正

--- tests/test_applianceControl.py
+++ tests/test_applianceControl.py
@@ -1,96 +1,127 @@
 import pytest
 import json
-#from Home-appliance-Control-AI 
-from applianceControl import *
-
+from flaskr.databases.collection_models.queueOperation import queueOperation
+from Home_appliance_control_AI.applianceControl import *
 
 # def test_config():
 #     assert not control().testing
 #     assert control({'TESTING': True}).testing
 
 
+@pytest.fixture(scope = 'function', autouse=True)
+def scope_function():#いったんmocker抜いた
+    #テスト前処理
+    #queueOperationを空にする
+    queueOperation.objects.all().delete()
+
+    print("setup before session")
+    yield
+    #テスト後処理
+
+    print("teardown after session")
+
+
+
 
 def test_controlTest_high_1():
-    #client = app.test_client()
+    
+    control(15, 20)
 
-    response = control("0", 15, 20)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
+    operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation.data, "big")
+    ope = hex(x)
+    operation.delete()
 
-    assert response == "上げる"
+    operation2 = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation2.data, "big")
+    ope2 = hex(x)
+    operation2.delete()
+
+    assert ope == "0x17b00ff"
+    assert ope2 == "0x17b10ef"
+
 
 
 def test_controlTest_high_2():
     #client = app.test_client()
 
-    response = control("0", 25, 30)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
+    control(25, 30)
+    operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation.data, "big")
+    ope = hex(x)
+    operation.delete()
 
-    assert response == "上げる"
+    operation2 = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation2.data, "big")
+    ope2 = hex(x)
+    operation2.delete()
+
+    assert ope == "0x17b00ff"
+    assert ope2 == "0x17b10ef"
 
 
 def test_controlTest_low_1():
     #client = app.test_client()
 
-    response = control("0", 25, 20)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
+    response = control(25, 20)
+    operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation.data, "big")
+    ope = hex(x)
 
-    assert response == "下げる"
+    operation.delete()
 
-
-def test_controlTest_low_2():
-    #client = app.test_client()
-
-    response = control("0", 20, 15)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
-
-    assert response == "下げる"
-
+    assert ope == "0x17b00ff"
 
 
 def test_controlTest_low_2():
     #client = app.test_client()
 
-    response = control("0", 20, 15)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
+    response = control(20, 15)
+    operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation.data, "big")
+    ope = hex(x)
 
-    assert response == "下げる"
+    operation.delete()
 
-
-
-def test_controlTest_true_1():
-    #client = app.test_client()
-
-    response = control("0", 20, 20)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
-
-    assert response == "tolerance"
-
-def test_controlTest_true_2():
-    #client = app.test_client()
-
-    response = control("0", 26, 23)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
-
-    assert response == "tolerance"
-
-
-def test_controlTest_true_3():
-    #client = app.test_client()
-
-    response = control("0", 18, 21)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
-
-    assert response == "tolerance"
+    assert ope == "0x17b00ff"
 
 
 
-def test_controlTest_outRange_1():
-    #client = app.test_client()
 
-    response = control("0", 18, 21)
-    #applianceControl.control(sNumber, int(tActual), tTarget)
+# def test_controlTest_suitable_1():
+#     #client = app.test_client()
 
-    assert response == "tolerance"
+#     response = control("0", 20, 20)
+#     #applianceControl.control(sNumber, int(tActual), tTarget)
+
+#     assert response == "tolerance"
+
+# def test_controlTest_suitable_2():
+#     #client = app.test_client()
+
+#     response = control("0", 26, 23)
+#     #applianceControl.control(sNumber, int(tActual), tTarget)
+
+#     assert response == "tolerance"
+
+
+# def test_controlTest_suitable_3():
+#     #client = app.test_client()
+
+#     response = control("0", 18, 21)
+#     #applianceControl.control(sNumber, int(tActual), tTarget)
+
+#     assert response == "tolerance"
+
+
+
+# def test_controlTest_outRange_1():
+#     #client = app.test_client()
+
+#     response = control(18, 21)
+#     #applianceControl.control(sNumber, int(tActual), tTarget)
+
+#     assert response == "tolerance"
 
 
 
